cb3dfe6778250ce1a1b0a83e10f95e387dca0caa
itedya <kontakt@itedya.com>
2021-12-26 21:30:14 +0100

    Added fetching authenticated user endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,28 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UseInterceptors
+} from "@nestjs/common";
 import { LoginDto } from "./dtos/login.dto";
 import { AuthService } from "./auth.service";
 import { UsersService } from "../users/users.service";
+import { JwtGuard } from "./guards/jwt.guard";
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller("/auth")
 export class AuthController {
   constructor(private authService: AuthService, private usersService: UsersService) {
+  }
+
+  @UseGuards(JwtGuard)
+  @Get("/user")
+  async user(@Req() request) {
+    return this.usersService.getByUsername(request.user.username);
   }
 
   @Post("/login")

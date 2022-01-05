0a733d228838a5cd7561a8531e790df80701a8ae
itedya <kontakt@itedya.com>
2022-01-05 09:51:08 +0100

    Added File input component

---
+++ client/src/components/Explorer/FileInput.vue
@@ -0,0 +1,9 @@
+<script setup>
+import FilePond, { generateServerOptions } from "../../composables/filepond-component.composable";
+
+const serverOptions = generateServerOptions();
+</script>
+
+<template>
+  <file-pond :server="serverOptions" />
+</template>

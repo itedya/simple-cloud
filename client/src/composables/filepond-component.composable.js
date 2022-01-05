9796a41557edce893c5d0a1261ef33ac46d53922
itedya <kontakt@itedya.com>
2022-01-05 09:50:45 +0100

    Added generating server options in filepond composable

--- client/src/composables/filepond-component.composable.js
+++ client/src/composables/filepond-component.composable.js
@@ -1,7 +1,22 @@
 import vueFilePond from 'vue-filepond';
-
+import { AuthStore } from "../store/auth.store";
 import 'filepond/dist/filepond.min.css';
 
 const FilePond = vueFilePond();
 
-export default FilePond;
+const generateServerOptions = () => {
+  return {
+    server: '/api/files',
+    chunkUploads: true,
+    process: {
+      headers: {
+        Authorization: `Bearer ${AuthStore.token}`
+      }
+    },
+  }
+}
+
+export {
+  generateServerOptions,
+  FilePond as default
+};

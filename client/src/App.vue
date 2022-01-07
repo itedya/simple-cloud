b65dd9c55ee28d1943bcdc6f681f62c632e2c39e
itedya <kontakt@itedya.com>
2022-01-07 05:14:17 +0100

    - Moved header into separate component - Copied route validation mechanism from footer to header - Created header navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <global-error-modal />
-  <header>
-    <h1>SimpleCloud</h1>
-  </header>
+  <app-header />
   <router-view />
   <app-footer />
   <iframe id="download-frame" style="display: none;"></iframe>
@@ -13,9 +11,10 @@ import { AuthStore } from "./store/auth.store";
 import GlobalErrorModal from "./components/GlobalErrorModal";
 import { startRouter } from "./router";
 import AppFooter from "./components/AppFooter";
+import AppHeader from "./components/AppHeader";
 
 export default {
-  components: { GlobalErrorModal, AppFooter },
+  components: { GlobalErrorModal, AppFooter, AppHeader },
 
   mounted() {
     AuthStore.fetchUser()
@@ -29,33 +28,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-@use "./scss/variables";
-
-header {
-  z-index: 100;
-
-  position: fixed;
-  top: 0;
-  left: 0;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  height: 60px;
-  width: 100%;
-
-  background-color: variables.$slate-800;
-  color: variables.$slate-50;
-
-  padding: 10px;
-
-  h1 {
-    font-family: Montserrat, sans-serif;
-    font-weight: 700;
-    font-size: variables.$text-3xl;
-  }
-}
-</style>
